ac675da295ea895645841150ba6c28ff07913a7b
Pacislavek <pacislavek@gmail.com>
2020-10-29 16:17:45 +0100

    general commit

---
+++ index.php
@@ -0,0 +1,74 @@
+<html lang="pl">
+
+<head>
+  <!-- meta -->
+  <title>Web007 - Szablon</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Patryk Kowalski | pacislavek@gmail.com">
+  <meta name="description" content="Szablon strony dla eksperta rodo.">
+  <meta http-equiv="Cache-control" content="public">
+  <meta name="robots" content="index, follow" />
+  <meta name="keywords" content="" />
+  <!-- og meta -->
+  <meta property="og:title" content="Web007 - Szablon" />
+  <meta property="og:url" content="" />
+  <meta property="og:description" content="Szablon strony dla eksperta rodo." />
+  <meta property="og:image" content="" />
+  <!-- font -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&family=Roboto:wght@300;400;500;700&family=Pacifico&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+  <!-- css -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/global.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
+
+<body data-spy="scroll" data-target="#navbar-spy" data-offset="73">
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+    <img src="media/go_consulting_logo_dark.png" width="125px" class="d-inline-block align-top" alt="" loading="lazy">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Usługi</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">O nas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Aduyt Rodo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Wdrożenie i dokumentacja</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kontakt</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+  <!-- SCRIPTS -->
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/smoothscroll.js"></script>
+
+  <script>
+    AOS.init();
+
+  </script>
+</body>
+
+
+</html>
